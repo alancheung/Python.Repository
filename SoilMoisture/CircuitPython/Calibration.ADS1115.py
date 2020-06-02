340d2e1a45909f7de140e17208aca7c0abebbf2f
Alan Cheung <cheung@rdacorp.com>
2020-06-02 00:57:44 -0400

    Allow user to skip calibration sequences

--- SoilMoisture/CircuitPython/Calibration.ADS1115.py
+++ SoilMoisture/CircuitPython/Calibration.ADS1115.py
@@ -26,50 +26,51 @@ chan = AnalogIn(ads, ADS.P0)
 # Dry calibration
 dryVal = []
 dryVolt = []
-input("[DRY] calibration. Press Enter to begin...")
-print("{:>5}\t{:>5}".format('raw', 'v'))
-while True:
-    val = chan.value
-    volt = chan.voltage
-    print("{:>5}\t{:>5.3f}".format(val, volt))
-    dryVal.append(val)
-    dryVolt.append(volt)
-    time.sleep(0.5)
-    if len(dryVal) >= 30:
-        break;
-
-print(f"Dry Value Average: {statistics.mean(dryVal)}")
-print(f"Dry Voltage Average: {statistics.mean(dryVolt)}")
+skip = input("[DRY] calibration. Press 'S' to skip and Enter to begin...")
+if (skip != "S"):
+    print("{:>5}\t{:>5}".format('raw', 'v'))
+    while True:
+        val = chan.value
+        volt = chan.voltage
+        print("{:>5}\t{:>5.3f}".format(val, volt))
+        dryVal.append(val)
+        dryVolt.append(volt)
+        time.sleep(0.5)
+        if len(dryVal) >= 30:
+            break;
+    dryValue = statistics.mean(dryVal)
+    dryVoltage = statistics.mean(dryVolt)
+    print(f"Dry Value Average: {dryValue}")
+    print(f"Dry Voltage Average: {dryVoltage}")
 
 # Wet calibration
 wetVal = []
 wetVolt = []
-input("[WET] calibration. Press Enter to begin...")
-print("{:>5}\t{:>5}".format('raw', 'v'))
-while True:
-    val = chan.value
-    volt = chan.voltage
-    print("{:>5}\t{:>5.3f}".format(val, volt))
-    wetVal.append(val)
-    wetVolt.append(volt)
-    time.sleep(0.5)
-    if len(wetVal) >= 30:
-        break;
+skip = input("[WET] calibration. Press 'S' to skip and Enter to begin...")
+if (skip != "S"):
+    print("{:>5}\t{:>5}".format('raw', 'v'))
+    while True:
+        val = chan.value
+        volt = chan.voltage
+        print("{:>5}\t{:>5.3f}".format(val, volt))
+        wetVal.append(val)
+        wetVolt.append(volt)
+        time.sleep(0.5)
+        if len(wetVal) >= 30:
+            break;
+    wetValue = statistics.mean(wetVal)
+    wetVoltage = statistics.mean(wetVolt)
+    print(f"Wet Value Average: {wetValue}")
+    print(f"Wet Voltage Average: {statistics.mean(wetVolt)}")
 
-
-dryValue = statistics.mean(dryVal)
-dryVoltage = statistics.mean(dryVolt)
-wetValue = statistics.mean(wetVal)
-wetVoltage = statistics.mean(wetVolt)
-print(f"Dry Value Average: {dryValue}")
-print(f"Dry Voltage Average: {dryVoltage}")
-print(f"Wet Value Average: {wetValue}")
-print(f"Wet Voltage Average: {statistics.mean(wetVolt)}")
 
 input("Note the values above. Press Enter to continue verification...")
-print("Value\tVoltage")
-while True:
-        val = calcValue(chan.value)
-        volt = calcVoltage(chan.voltage)
-        print(f"{val}\t{volt}")
-        time.sleep(0.5)
+if (len(wetVal) > 0 and len(dryVal) > 0):
+    print("Value\tVoltage")
+    while True:
+            val = calcValue(chan.value)
+            volt = calcVoltage(chan.voltage)
+            print(f"{val}\t{volt}")
+            time.sleep(0.5)
+else:
+    print("No calibration values to display.")
