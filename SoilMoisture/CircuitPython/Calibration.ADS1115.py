cecfbb8d4eb82d0b78f6e20afbef47008a5ebbd3
Alan Cheung <cheung@rdacorp.com>
2020-06-07 01:13:24 -0400

    Write calibration values to a json file.

--- SoilMoisture/CircuitPython/Calibration.ADS1115.py
+++ SoilMoisture/CircuitPython/Calibration.ADS1115.py
@@ -4,9 +4,17 @@ import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import statistics
+import json
+import argparse
+import os
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-o", "--output", default="calibration", help="The file name of the calibrated json.")
+args = vars(argParser.parse_args())
+outputFile = args["output"]
 
 def percent(num, high, low):
-    return 100 * float(num - low) / float(high - low)
+    return 100 * (float(num - low) / float(high - low))
 
 def calcValue(value):
     return percent(value, dryValue, wetValue)
@@ -61,16 +69,37 @@ if (skip != "S"):
     wetValue = statistics.mean(wetVal)
     wetVoltage = statistics.mean(wetVolt)
     print(f"Wet Value Average: {wetValue}")
-    print(f"Wet Voltage Average: {statistics.mean(wetVolt)}")
+    print(f"Wet Voltage Average: {wetVoltage}")
 
-
+# Continue the calibration and show computed values.
 input("Note the values above. Press Enter to continue verification...")
 if (len(wetVal) > 0 and len(dryVal) > 0):
-    print("Value\tVoltage")
+    # Dump the settings to a file.
+    calibratedObj = {
+        "DryValue": dryValue,
+        "DryVoltage": dryVoltage,
+        "WetValue": wetValue,
+        "WetVoltage": wetVoltage
+        }
+    fileName = f"./{outputFile}.json"
+    if os.path.isfile(fileName):
+        with open(fileName, "r+") as outputFile:
+            data = outputFile.read()
+            outputFile.seek(0)
+            json.dump(calibratedObj, outputFile)
+            outputFile.truncate()
+    else:
+        with open(fileName, "w") as outputFile:
+            json.dump(calibratedObj, outputFile)
+    print(f"Saved calibration values to {fileName}")
+
+    print("Value\t\t\tVoltage")
     while True:
-            val = calcValue(chan.value)
-            volt = calcVoltage(chan.voltage)
-            print(f"{val}\t{volt}")
-            time.sleep(0.5)
+        rawVal = chan.value
+        rawVolt = chan.voltage
+        val = calcValue(rawVal)
+        volt = calcVoltage(rawVolt)
+        print(f"{rawVal} ({round(val, 4)}%)\t\t\t{rawVolt} ({round(volt, 4)}%)")
+        time.sleep(0.5)
 else:
     print("No calibration values to display.")
