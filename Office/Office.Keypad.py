0b36844f1f59f0fc0ca8802e07253238ab272966
Alan Cheung <cheung@rdacorp.com>
2020-08-20 21:09:57 -0400

    Initial commit of new keypad project.

---
+++ Office/Office.Keypad.py
@@ -0,0 +1,50 @@
+# ------------------------- DEFINE IMPORTS ---------------------------
+from __future__ import print_function
+from datetime import datetime
+import argparse
+
+# ------------------------- DEFINE ARGUMENTS -------------------------
+# argParser.add_argument("-a", "--min-area", type=int, default=500, help="Minimum area size before motion detection")
+#argParser.add_argument('--ononly', dest='ononly', action='store_true', help="Disable turning lights off command")
+#argParser.add_argument('--remote', dest='interactive', action='store_false', help="Disable Pi hardware specific functions")
+#argParser.set_defaults(interactive=True)
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument('--quiet', dest='quiet', action='store_true', help="Disable logging")
+argParser.add_argument("-f", "--log-file", type=string, default=None, help="Specify file to log to.")
+argParser.set_defaults(quiet=False)
+
+args = vars(argParser.parse_args())
+quiet = args["quiet"]
+logFileName = args["log_file"]
+
+# ------------------------- DEFINE GLOBALS ---------------------------
+
+# ------------------------- DEFINE FUNCTIONS -------------------------
+def log(text, displayWhenQuiet = False):
+    if displayWhenQuiet or not quiet:
+        now = datetime.now().strftime("%H:%M:%S")
+        message = f"{now}: {text}"
+        if logFileName is not None:
+            with open(f"/home/pi/Project/{logFileName}", "a") as fout:
+                fout.write(f"{message}\n")
+        else:
+            print(message)
+
+def err(text):
+    log(text, True)
+
+def alrt(text):
+    log(text, True)
+
+# ------------------------- DEFINE INITIALIZE ------------------------
+log("Initializing...", displayWhenQuiet = True)
+log(f"Args: {args}", displayWhenQuiet = True)
+
+# ------------------------- DEFINE RUN -------------------------------
+log("Initialized!", displayWhenQuiet = True)
+log("Running...", displayWhenQuiet = True)
+try:
+    log("Run")
+except KeyboardInterrupt:
+    log("KeyboardInterrupt caught! Cleaning up...")
