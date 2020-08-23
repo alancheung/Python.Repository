3c72032d07f9764c420e0060b61023f322b515cd
Alan Cheung <cheung@rdacorp.com>
2020-08-23 15:29:02 -0400

    Add GPIO test code.

--- Common/gpioTest.py
+++ Common/gpioTest.py
@@ -16,8 +16,3 @@ try:
         high = not high
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-#import PySimpleGUI as sg
-#layout = [[sg.Text('Hello!')]]
-#window = sg.Window('Keypad', layout, no_titlebar=True, location=(0,0), size=(800, 400), keep_on_top=False).Finalize()
-#print(window.GetScreenDimensions())
