24bcf95344009178c0145fe2762c7a5b8281b07b
Alan Cheung <cheung@rdacorp.com>
2020-11-22 23:38:31 -0500

    Implement request to localhost.

--- Cronjobs/LightsAPI.cronjob.py
+++ Cronjobs/LightsAPI.cronjob.py
@@ -5,30 +5,27 @@ Send an API request to localhost LIFX controller and invoke the command.
 from __future__ import print_function
 from datetime import datetime
 import argparse
+import json
+import requests
 
 # ------------------------- DEFINE ARGUMENTS -------------------------
-# argParser.add_argument("-a", "--min-area", type=int, default=500, help="Minimum area size before motion detection")
-#argParser.add_argument('--ononly', dest='ononly', action='store_true', help="Disable turning lights off command")
-#argParser.add_argument('--remote', dest='interactive', action='store_false', help="Disable Pi hardware specific functions")
-#argParser.set_defaults(interactive=True)
-
 argParser = argparse.ArgumentParser()
 argParser.add_argument('--quiet', dest='quiet', action='store_true', help="Disable logging")
-argParser.add_argument("-f", "--log-file", type=string, default=None, help="Specify file to log to.")
-argParser.add_argument("-a", "--args", type=string, required=True, help="JSON request to send.")
+argParser.add_argument("-f", "--log-file", default=None, help="Specify file to log to.")
+argParser.add_argument("-r", "--request", required=True, help="JSON request to send.")
 argParser.set_defaults(quiet=False)
 
 args = vars(argParser.parse_args())
 quiet = args["quiet"]
 logFileName = args["log_file"]
-argument = args["args"]
+request = args["request"]
 
 # ------------------------- DEFINE GLOBALS ---------------------------
 
 # ------------------------- DEFINE FUNCTIONS -------------------------
 def log(text, displayWhenQuiet = False):
     if displayWhenQuiet or not quiet:
-        now = datetime.now().strftime("%H:%M:%S")
+        now = datetime.now().strftime("%x %X")
         message = f"{now}: {text}"
         if logFileName is not None:
             with open(f"/home/pi/Project/{logFileName}", "a") as fout:
@@ -42,6 +39,17 @@ def err(text):
 def alrt(text):
     log(text, True)
 
+def sendLightRequest(command):
+    server = 'http://localhost:3000/api/lifx'
+    try:
+        log(f'Sending light request post to {server}')
+        req = requests.post(f'{server}', data = command, timeout=30)
+        if (req.status_code != 200):
+            err(f"Request status code did not indicate success ({req.status_code})!");
+    except Exception as ex:
+        err(f"Could not send light request to '{server}' due to {str(ex)}!")
+        successful = False
+
 # ------------------------- DEFINE INITIALIZE ------------------------
 log("Initializing...", displayWhenQuiet = True)
 log(f"Args: {args}", displayWhenQuiet = True)
@@ -51,5 +59,9 @@ log("Initialized!", displayWhenQuiet = True)
 log("Running...", displayWhenQuiet = True)
 try:
     log("Run")
+    parsedRequest = json.loads(request)
+
+    log(f"Parsed {parsedRequest}")
+    sendLightRequest(parsedRequest)
 except KeyboardInterrupt:
     log("KeyboardInterrupt caught! Cleaning up...")
