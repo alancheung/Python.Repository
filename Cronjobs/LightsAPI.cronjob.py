cd160c58c2bf71eadaa1be3cd88dd66c3319c164
Alan Cheung <cheung@rdacorp.com>
2020-11-22 22:32:54 -0500

    Add new LightsAPI.cronjob.py and remove files moved to other repositories.

--- Cronjobs/LightsAPI.cronjob.py
+++ Cronjobs/LightsAPI.cronjob.py
@@ -1,18 +1,10 @@
-
 '''
-Simple open program with no authentication
+Send an API request to localhost LIFX controller and invoke the command.
 '''
 # ------------------------- DEFINE IMPORTS ---------------------------
 from __future__ import print_function
 from datetime import datetime
 import argparse
-import PySimpleGUI as sg
-import time
-try:
-    import RPi.GPIO as GPIO
-except RuntimeError:
-    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
-
 
 # ------------------------- DEFINE ARGUMENTS -------------------------
 # argParser.add_argument("-a", "--min-area", type=int, default=500, help="Minimum area size before motion detection")
@@ -22,16 +14,14 @@ except RuntimeError:
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument('--quiet', dest='quiet', action='store_true', help="Disable logging")
-argParser.add_argument("-f", "--log-file", default=None, help="Specify file to log to.")
-argParser.add_argument("-p", "--relay-pin", type=int, default=4, help="GPIO number that sensor is connected to.")
-argParser.add_argument("-o", "--open-time", type=int, default=3, help="Number of seconds to keep relay open (and lock unlocked).")
+argParser.add_argument("-f", "--log-file", type=string, default=None, help="Specify file to log to.")
+argParser.add_argument("-a", "--args", type=string, required=True, help="JSON request to send.")
 argParser.set_defaults(quiet=False)
 
 args = vars(argParser.parse_args())
 quiet = args["quiet"]
 logFileName = args["log_file"]
-relayPin = args["relay_pin"]
-openTime = args["open_time"]
+argument = args["args"]
 
 # ------------------------- DEFINE GLOBALS ---------------------------
 
@@ -52,47 +42,14 @@ def err(text):
 def alrt(text):
     log(text, True)
 
-def open_sesame():
-    window[passwordKey].update('ACCESS GRANTED')
-    
-    # Connect NC relay connections and open door.
-    GPIO.output(relayPin, GPIO.HIGH)
-    time.sleep(openTime)
-    GPIO.output(relayPin, GPIO.LOW)
-
 # ------------------------- DEFINE INITIALIZE ------------------------
 log("Initializing...", displayWhenQuiet = True)
 log(f"Args: {args}", displayWhenQuiet = True)
-
-layout = [[sg.Submit('OPEN', size=(600, 460))]]
-log("GUI layout set!")
-
-GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-GPIO.setup(relayPin, GPIO.OUT) # GPIO Assign mode
-log("GPIO initialized!")
 
 # ------------------------- DEFINE RUN -------------------------------
 log("Initialized!", displayWhenQuiet = True)
 log("Running...", displayWhenQuiet = True)
 try:
-    showKeypad = True
-    #, no_titlebar=True, location=(0,0), size=piTouchSize, keep_on_top=True
-    window = sg.Window('Error', layout, no_titlebar=True, location=(0,0), size=(800, 480), keep_on_top=False).Finalize()
-
-    while showKeypad:
-        event, values = window.read()
-        log(event)
-
-        if event in ('Submit', 'OPEN'):
-            open_sesame()
-
-        # This captures the Ctrl-C and exit button events
-        elif event in (sg.WIN_CLOSED, 'Quit'):
-            #sg.popup("Closing")
-            break
-
+    log("Run")
 except KeyboardInterrupt:
     log("KeyboardInterrupt caught! Cleaning up...")
-finally:
-    GPIO.cleanup()
-    log("Program exiting...")
